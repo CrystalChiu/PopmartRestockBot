6171330a901fdb656f29f0ed53a8d947798b1183
Crystal Chiu <chiubacca@Crystals-MacBook-Air.local>
2025-05-05 19:41:16 -0700

    Stop tracking config.js

--- src/config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    BASE_URL: "https://www.popmart.com/us/search/labubu?page=",
-    CHECK_INTERVAL: 6000, // 1 minute base time (will add variability of +- 10 seconds)
-    CHANNEL_ID: "something something",
-    EXCLUDED_PRODUCTS: [],
-}
